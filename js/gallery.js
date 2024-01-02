2289ee550a34027193b7c84af679284f08936a67
KazukiAsuna <138202531+KazukiAsuna@users.noreply.github.com>
2024-01-02 21:29:50 +0530

    improved UI

--- js/gallery.js
+++ js/gallery.js
@@ -1,42 +1,59 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Number of artworks
     const numArtworks = 18;
-
-    // Reference to the gallery container
     const galleryContainer = document.getElementById('galleryContainer');
+    const seeMoreButton = document.getElementById('seeMoreButton');
+    let displayedArtworks = 6; // Initial number of displayed artworks
 
-    // Loop to create gallery cards
-    for (let i = 1; i <= numArtworks; i++) {
-        // Create card div
+    function createGalleryCard(index) {
         const cardDiv = document.createElement('div');
         cardDiv.className = 'card';
         cardDiv.href = '#';
 
-        // Create gallery info div
         const galleryInfoDiv = document.createElement('div');
         galleryInfoDiv.className = 'gallery-info';
 
-        // Create gallery bio div
         const galleryBioDiv = document.createElement('div');
         galleryBioDiv.className = 'gallery-bio';
 
-        // Set h3 and p content
         const heading = document.createElement('h3');
-        heading.textContent = 'Artwork ' + i;
+        heading.textContent = 'Artwork ' + index;
         const paragraph = document.createElement('p');
-        paragraph.textContent = 'Generative Art using Advanced AI';
+        paragraph.textContent = 'Generative Art using BlueMoon AI';
 
-        // Append h3 and p to gallery bio div
         galleryBioDiv.appendChild(heading);
         galleryBioDiv.appendChild(paragraph);
 
-        // Append gallery bio div to gallery info div
         galleryInfoDiv.appendChild(galleryBioDiv);
-
-        // Append gallery info div to card div
         cardDiv.appendChild(galleryInfoDiv);
 
-        // Append card div to gallery container
         galleryContainer.appendChild(cardDiv);
     }
+
+    function displayArtworks(startIndex, endIndex) {
+        for (let i = startIndex; i < endIndex; i++) {
+            createGalleryCard(i + 1);
+        }
+    }
+
+    // Initial display
+    displayArtworks(0, displayedArtworks);
+
+    seeMoreButton.addEventListener('click', function () {
+        if (displayedArtworks < numArtworks) {
+            const nextDisplayedArtworks = Math.min(displayedArtworks + 6, numArtworks);
+            displayArtworks(displayedArtworks, nextDisplayedArtworks);
+            displayedArtworks = nextDisplayedArtworks;
+
+        }
+        else {
+
+            if (displayedArtworks === numArtworks) {
+                // If all artworks are displayed, change button behavior
+                seeMoreButton.href = 'https://github.com/orgs/BlueMoonAI/discussions';
+                seeMoreButton.target = '_blank';
+            }
+        }
+
+
+    });
 });
